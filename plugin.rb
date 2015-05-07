a12be6798075a64da6fa12e94a910e3aa6525b13
Dashamir Hoxha <dashohoxha@gmail.com>
2015-05-07 19:49:56 +0200

    Inserting the javascript and the css code.

--- plugin.rb
+++ plugin.rb
@@ -3,3 +3,11 @@
 # version: 0.1
 # authors: Dashamir Hoxha
 
+if params[:embedded] == 'true'
+  register_asset "javascripts/iframeResizer.contentWindow.min.js", :server_side
+
+  register_css <<CSS
+
+CSS
+
+end
